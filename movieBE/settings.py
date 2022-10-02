2c9779518e03e9aa2105747c837f7035869f162e
Thilda03 <93349873+Thilda03@users.noreply.github.com>
2022-10-02 16:06:46 -0500

    update

--- movieBE/settings.py
+++ movieBE/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
+
 
 REST_FRAMEWORK = {
 # Use Django's standard `django.contrib.auth` permissions,
